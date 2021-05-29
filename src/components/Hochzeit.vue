90fb9ae7fc82b78d9bc7a6eb089db32c0bad45be
Ludwig Sonnenberg <info@ludwigso.de>
2021-05-29 09:32:18 +0200

    Minor text change

--- src/components/Hochzeit.vue
+++ src/components/Hochzeit.vue
@@ -11,8 +11,8 @@
         <br><br>
         Achtung: Euer Browser wird euch möglicherweise folgenden Fehler anzeigen "Dies ist keine sichere Verbindung". Etwas versteckt könnt ihr nach einem Klick auf "Erweitert" und dann "Weiter zu breitix.selfhost.eu (unsicher)" trotzdem auf die Seite navigieren.
         <br><br>        
-        Der Hintergrund: Der Server auf dem die Fotos gespeichert sind steht in Schmöckwitz und wird von meinem Vater betrieben. Euer Browser erwartet ein Zertifikat von einer "offiziellen" Stelle, welches bestätigt wem dieser Server gehört und eine verschlüsselte Verbindung ermöglicht. Dieses Zertifikat ist hier selbst ausgestellt damit die Verschlüsselung nach dem Standard im Internet funktioniert. Euer Browser zweifelt aber die Identität des Ausstellers an, weil es keine "offizielle" Bestätigung gibt.
-      </div>
+        Der Hintergrund: Der Server auf dem die Fotos gespeichert sind steht in Schmöckwitz und wird von meinem Vater betrieben. Euer Browser erwartet ein Zertifikat von einer "offiziellen" Stelle, welches eine verschlüsselte Verbindung ermöglicht und bestätigt wem dieser Server gehört. Dieses Zertifikat ist hier selbst ausgestellt, damit die Verschlüsselung nach dem Standard im Internet funktioniert. Euer Browser zweifelt jedoch die Identität des Ausstellers an, weil es keine "offizielle" Bestätigung gibt. 
+        </div>
         <br>
     </div>
   </div>
