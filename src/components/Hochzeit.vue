b946cbe2f85ed65f8a5f2fcda200710370d28811
Ludwig Sonnenberg <info@ludwigso.de>
2021-05-29 09:27:13 +0200

    Update text with warning for unsafe connection

--- src/components/Hochzeit.vue
+++ src/components/Hochzeit.vue
@@ -4,7 +4,16 @@
     <v-subheader>Hochzeitsfotos</v-subheader>
     <v-divider> </v-divider>
     <div class="text">
+      <div class="link">
       <a href="https://breitix.selfhost.eu:12346/index.php/s/c9DNQeseC82ds6S">Hier geht es zu den Hochzeitsfotos</a>
+      </div>
+      <div>
+        <br><br>
+        Achtung: Euer Browser wird euch möglicherweise folgenden Fehler anzeigen "Dies ist keine sichere Verbindung". Etwas versteckt könnt ihr nach einem Klick auf "Erweitert" und dann "Weiter zu breitix.selfhost.eu (unsicher)" trotzdem auf die Seite navigieren.
+        <br><br>        
+        Der Hintergrund: Der Server auf dem die Fotos gespeichert sind steht in Schmöckwitz und wird von meinem Vater betrieben. Euer Browser erwartet ein Zertifikat von einer "offiziellen" Stelle, welches bestätigt wem dieser Server gehört und eine verschlüsselte Verbindung ermöglicht. Dieses Zertifikat ist hier selbst ausgestellt damit die Verschlüsselung nach dem Standard im Internet funktioniert. Euer Browser zweifelt aber die Identität des Ausstellers an, weil es keine "offizielle" Bestätigung gibt.
+      </div>
+        <br>
     </div>
   </div>
 </template>
@@ -19,10 +28,14 @@ export default Vue.extend({
 
 <style scoped>
   .text {
-    text-align: center;
     padding: 12px 16px 0 16px; /* same as v-list on knowledge site*/
-    font-size: 25px;
+    font-size: 14px;
     margin-top: 50px;
     min-height: 150px;
+  }
+
+  .link {
+    text-align: center;
+    font-size: 25px;
   }
 </style>
