cefb57dea7d2afd6a320a685a2738013e53704f6
Ludwig Sonnenberg <info@ludwigso.de>
2021-05-31 22:51:12 +0200

    Remove disclaimer due to being obsolete

--- src/components/Hochzeit.vue
+++ src/components/Hochzeit.vue
@@ -3,17 +3,8 @@
     <div class="top-padding-without-list"></div>
     <v-subheader>Hochzeitsfotos</v-subheader>
     <v-divider> </v-divider>
-    <div class="text">
-      <div class="link">
+    <div class="link">
       <a href="https://nc.breitsprecher.net:12346/index.php/s/c9DNQeseC82ds6S">Hier geht es zu den Hochzeitsfotos</a>
-      </div>
-      <div>
-        <br><br>
-        Achtung: Euer Browser wird euch möglicherweise folgenden Fehler anzeigen "Dies ist keine sichere Verbindung". Etwas versteckt könnt ihr nach einem Klick auf "Erweitert" und dann "Weiter zu breitix.selfhost.eu (unsicher)" trotzdem auf die Seite navigieren.
-        <br><br>        
-        Der Hintergrund: Der Server auf dem die Fotos gespeichert sind steht in Schmöckwitz und wird von meinem Vater betrieben. Euer Browser erwartet ein Zertifikat von einer "offiziellen" Stelle, welches eine verschlüsselte Verbindung ermöglicht und bestätigt wem dieser Server gehört. Dieses Zertifikat ist hier selbst ausgestellt, damit die Verschlüsselung nach dem Standard im Internet funktioniert. Euer Browser zweifelt jedoch die Identität des Ausstellers an, weil es keine "offizielle" Bestätigung gibt. 
-        </div>
-        <br>
     </div>
   </div>
 </template>
@@ -27,14 +18,10 @@ export default Vue.extend({
 </script>
 
 <style scoped>
-  .text {
-    padding: 12px 16px 0 16px; /* same as v-list on knowledge site*/
-    font-size: 14px;
-    margin-top: 50px;
-    min-height: 150px;
-  }
-
   .link {
+    padding: 0px 16px 0 16px;
+    margin-top: 60px;
+    min-height: 120px;
     text-align: center;
     font-size: 25px;
   }
